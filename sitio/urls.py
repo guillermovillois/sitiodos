afce06463ba7e396e86a022d5846fd6c4df819e0
Guille <guillearg@gmail.com>
2017-09-10 14:33:36 -0300

    nuevo url

--- sitio/urls.py
+++ sitio/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^twitter/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
     url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^u/', include('profiles.urls', namespace='profiles')),
